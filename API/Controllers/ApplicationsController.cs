527e35a6b11060e1a81770b6682c7f5cae5ce568
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-05-06 21:40:25 +0200

    Add initial fake API for applications GET

---
+++ API/Controllers/ApplicationsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MapHive.Server.API.Controllers
+{
+    [Route("applications")]
+    public class ApplicationsController : ApiController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
+        {
+            //TODO - first check if user is known, so can customise output - public apps vs apps per user!
+            
+            //TODO - for unauthenticated users, when getting the apps to display in the app switcher only return the apps that are public, common or something
+
+            return Ok(MapHive.Server.Core.Application.Read());
+        }
+    }
+}
--- Core/Application/Read.cs
+++ Core/Application/Read.cs
@@ -22,17 +22,17 @@ namespace MapHive.Server.Core
                 new dm.Application()
                 {
                     Id = Guid.Parse("e645c160-e03d-40cf-8a5f-35eac0be7118"),
-                    ShortName = "app1_id",
+                    ShortName = "app1",
                     Name ="App 1", 
                     Description = "A test HOST app that suppresses nested framed apps",
-                    Url = "https://app1.emapa.local/?suppressnested=true#some/hash/123/456"
+                    Url = "https://app1.maphive.local/?suppressnested=true#some/hash/123/456"
                 },
                 new dm.Application()
                 {
                     Id = Guid.Parse("58ec63c7-afc9-4ba6-86be-c4822034c086"),
                     //no short name, so can test id in the url part!
                     Name ="App 2",
-                    Url = "https://app2.emapa.local/?param=test param so can be sure parameterised app urls also work&suppressnested=true",
+                    Url = "https://app2.maphive.local/?param=test param so can be sure parameterised app urls also work&suppressnested=true",
                     UseSplashScreen = true,
                     RequiresAuth = true
                 },
@@ -41,7 +41,7 @@ namespace MapHive.Server.Core
                     Id = Guid.Parse("9e263268-2da3-4134-9f05-fa65dfab44b6"),
                     ShortName = "app3x3",
                     Name ="App 3",
-                    Url = "https://app3.emapa.local/",
+                    Url = "https://app3.maphive.local/",
                     RequiresAuth = true
                 },
                 new dm.Application()
@@ -50,7 +50,7 @@ namespace MapHive.Server.Core
                     ShortName = "hgis1",
                     Name ="HGIS v1",
                     Description = "An initial port of the Cartomatic\'s HGIS",
-                    Url = "https://hgis.emapa.local/"
+                    Url = "https://hgis.maphive.local/"
                 }
             };
         }
