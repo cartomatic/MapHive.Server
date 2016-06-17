eae7c38641dce46705d43cd4bd7220ba092a35e8
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-06-17 19:59:52 +0200

    Start base class - does not compile

---
+++ Core/Data/Base/Ctor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapHive.Server.Core.Interfaces;
+
+namespace MapHive.Server.Core.Data
+{
+    public abstract partial class Base : IBase, IValidate
+    {
+        /// <summary>
+        /// Type identifier - used to establish links between objects. not saved in a database;
+        /// declared via class constructor.
+        /// Important: when uuid is changed in code it will affect all the links in the database(s)
+        /// </summary>
+        public static Guid TypeUuid { get; private set; }
+
+        static Base()
+        {
+            TypeUuid = default(Guid);
+        }
+
+        public Base()
+        {
+            if(TypeUuid == default (Guid))
+                throw new Exception("When deriving from MapHive.Server.Data.Base make sure to provide a unique type identifier!");
+        }
+
+    }
+}
