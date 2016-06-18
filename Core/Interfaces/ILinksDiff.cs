6f92c7f36768610d3c19e66f75d6a0d0f4f2ae38
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-06-18 06:02:22 +0200

    Add some interfaces

---
+++ Core/Interfaces/ILinksDiff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapHive.Server.Core.Interfaces
+{
+    interface ILinksDiff
+    {
+        /// <summary>
+        /// Link objects to be either inserted or updated
+        /// </summary>
+        IEnumerable<ILink> Upsert { get; set; }
+
+        /// <summary>
+        /// Link objects to be destroyed
+        /// </summary>
+        IEnumerable<Guid> Destroy { get; set; } 
+    }
+}
---
+++ Core/Interfaces/IValidate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace MapHive.Server.Core.Interfaces
+{
+    /// <summary>
+    /// Whether or not a class exposes functionality to validate its data model
+    /// </summary>
+    public interface IValidate
+    {
+        /// <summary>
+        /// Gets the validator
+        /// </summary>
+        /// <returns></returns>
+        IValidator GetValidator();
+
+        /// <summary>
+        /// Validates data model. Should throw when model is not valid
+        /// </summary>
+        void Validate();
+    }
+}
