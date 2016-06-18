254a8e52af5e23286f36eca3a7a231a17ee8ad17
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-06-18 06:05:24 +0200

    FluentValidation based model validation

---
+++ Core/Data/Validation/ValidationErrors.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.Validators;
+using MapHive.Server.Core.Interfaces;
+
+namespace MapHive.Server.Core.Validation
+{
+    /// <summary>
+    /// Validation errors
+    /// </summary>
+    public static class ValidationErrors
+    {
+        public static IValidationError ValueRequired { get; } = new ValidationError()
+        {
+            Code = "value_required",
+            Message = "Value for the field is required."
+        };
+
+        public static IValidationError InvalidLength { get; } = new ValidationError()
+        {
+            Code = "invalid_length",
+            Message = "Invalid field length. Min lenght is: {MinLength}, max length is: {MaxLength}, supplied length was {TotalLength}"
+        };
+
+        public static IValidationError InvalidEmail { get; } = new ValidationError()
+        {
+            Code = "invalid_email",
+            Message = "Invalid email."
+        };
+
+        public static IValidationError EmailInUse { get; } = new ValidationError
+        {
+            Code = "email_in_use",
+            Message = "Email in use."
+        };
+
+        public static IValidationError UnknownError { get; } = new ValidationError
+        {
+            Code = "unknown_error",
+            Message = "Unknown error."
+        };
+
+    }
+}
---
+++ Core/Data/Validation/ValidationFailedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapHive.Server.Core.Interfaces;
+
+namespace MapHive.Server.Core.Validation
+{
+    public class ValidationFailedException : Exception
+    {
+        public IList<IValidationError> ValidationErrors { get; set; } = new List<IValidationError>();
+    } 
+}
