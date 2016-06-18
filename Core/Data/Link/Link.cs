59e5d74d700055a0bc5e595980e026d9d334b35d
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-06-18 06:05:05 +0200

    Base Links abstraction

---
+++ Core/Data/Link/Link.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapHive.Server.Core.Interfaces;
+
+namespace MapHive.Server.Core.Data
+{
+    /// <summary>
+    /// Link object used to express relations between objects
+    /// </summary>
+    public class Link : ILink
+    {
+        /// <summary>
+        /// Link identifier (primary key in relationships table)
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Unique identifier for object that contains links; direction of a link is from parent to child, although obviously the dataset as such can bq querried the other way round too
+        /// </summary>
+        public Guid ParentUuid { get; set; }
+
+        /// <summary>
+        /// Unique identifier for object that is linked to parent object
+        /// </summary>
+        public Guid ChildUuid { get; set; }
+
+        /// <summary>
+        /// Parent's type unique identifier
+        /// </summary>
+        public Guid ParentTypeUuid { get; set; }
+
+        /// <summary>
+        /// Child's type unique identifier
+        /// </summary>
+        public Guid ChildTypeUuid { get; set; }
+
+        /// <summary>
+        /// Extra JSON serialised data
+        /// </summary>
+        public string Data { get; set; }
+
+        /// <summary>
+        /// Sort order if any
+        /// </summary>
+        public int? SortOrder { get; set; }
+    }
+}
---
+++ Core/Data/LinksDiff/LinksDiff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapHive.Server.Core.Interfaces;
+
+namespace MapHive.Server.Core.Data
+{
+    /// <summary>
+    /// Decribes changes in the object links. Changes must be explicit in order to modify the links
+    /// </summary>
+    public class LinksDiff : ILinksDiff
+    {
+        /// <summary>
+        /// Link objects to be either inserted or updated
+        /// </summary>
+        public IEnumerable<ILink> Upsert { get; set; } = new List<Link>(); 
+
+        /// <summary>
+        /// Link objects to be destroyed
+        /// </summary>
+        public IEnumerable<Guid> Destroy { get; set; } = new List<Guid>();
+    }
+}
