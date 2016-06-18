918148f995565cbc070a16656e57d6b95c954285
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-06-18 06:04:12 +0200

    Adjust some assembly info

--- API/Properties/AssemblyInfo.cs
+++ API/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("API")]
+[assembly: AssemblyTitle("MapHive.Server.API")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("API")]
+[assembly: AssemblyCompany("Cartomatic")]
+[assembly: AssemblyProduct("MapHive.Server.API")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
