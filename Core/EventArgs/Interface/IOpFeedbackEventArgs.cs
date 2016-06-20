8eb43eeec8fc4355e94caeea043679aa08496c32
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-06-20 20:09:53 +0200

    Add OpFeedback event args

---
+++ Core/EventArgs/Interface/IOpFeedbackEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapHive.Server.Core.EventArgs.Interface
+{
+    public interface IOpFeedbackEventArgs
+    {
+        /// <summary>
+        /// Dict containing feedback on the operation performed.
+        /// </summary>
+        IDictionary<string, object> OperationFeedback { get; set; }
+    }
+}
---
+++ Core/EventArgs/OpFeedbackEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapHive.Server.Core.EventArgs.Interface;
+
+namespace MapHive.Server.Core.EventArgs
+{
+    /// <summary>
+    /// Operation feedbac event arguments
+    /// </summary>
+    public class OpFeedbackEventArgs : System.EventArgs, IOpFeedbackEventArgs
+    {
+        /// <summary>
+        /// Dict containing feedback on the operation performed.
+        /// </summary>
+        public IDictionary<string, object> OperationFeedback { get; set; } = new Dictionary<string, object>();
+    }
+}
