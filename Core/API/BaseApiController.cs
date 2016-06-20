c9f32a7584c48a14d5c2bf901f515ba170407737
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-06-20 20:24:50 +0200

    Start abstract WebAPI base controller

---
+++ Core/API/BaseApiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using MapHive.Server.Core.DataModel.Interface;
+
+namespace MapHive.Server.Core.API
+{
+    /// <summary>
+    /// Provides the base for the Web APIs that expose IBase like objects via RESTful ike API
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BaseApiController<T> : ApiController where T : IBase
+    {
+        /// <summary>
+        /// Database context to be used
+        /// </summary>
+        protected DbContext _dbCtx { get; private set; }
+
+        public BaseApiController(DbContext dbCtx, DbContext dbCtx1)
+        {
+            _dbCtx = dbCtx1;
+        }
+
+
+
+
+    }
+}
