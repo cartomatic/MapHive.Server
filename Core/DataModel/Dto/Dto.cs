311996170911db0cdddfcff153d8a0a629638eef
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-06-21 21:44:39 +0200

    Add Automated Dto funtionality to core

---
+++ Core/DataModel/Dto/Dto.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapHive.Server.Core.DataModel.Interface;
+
+namespace MapHive.Server.Core.DataModel
+{
+    // <summary>
+    /// When in a need to transfer some modified objects, but still closely mapped to the Core.DataModel.Base, this class provides the required properties;
+    /// It should be kept intact with the Core.DataModel.Base, or just IBase
+    /// </summary>
+    public class Dto<TDto> : IBase, IDto<TDto>
+        where TDto : class
+    {
+        public Guid TypeUuid { get; set; }
+        public Guid Uuid { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? LastModifiedBy { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        //hide links behind explicit implementation
+        ILinksDiff IBase.Links { get; set; }
+
+        /// <summary>
+        /// Creates an instance of object from a DTO class
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public T FromDto<T>(TDto o) where T : class
+        {
+            var core = o.CopyPublicPropertiesToNew<T>();
+
+            FromDto(o, core);
+
+            return core;
+        }
+
+        /// <summary>
+        /// Converts instance of class to its DTO
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public TDto ToDto<T>(T o) where T : class
+        {
+            var dto = o.CopyPublicPropertiesToNew<TDto>();
+
+            ToDto(o, dto);
+
+            return dto;
+        }
+
+        /// <summary>
+        /// Customised data operations when transferring from DTO
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="src"></param>
+        /// <param name="dest"></param>
+        protected virtual void FromDto<T>(TDto src, T dest)
+        {
+            //do some hocus pocus data transfer in a derived class!
+        }
+
+        /// <summary>
+        /// Customised data operations when transferring to DTO
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="src"></param>
+        /// <param name="dest"></param>
+        protected virtual void ToDto<T>(T src, TDto dest)
+        {
+            //do some hocus pocus data transfer in a derived class!
+        }
+
+        protected void CheckTypes<T1, T2>()
+        {
+            if (typeof(T1) != typeof(T2))
+                throw new ArgumentException($"Invalid DataModel<->DTO types; Expected {typeof(T1).Name}, received {typeof(T2).Name}");
+        }
+    }
+}
---
+++ Core/DataModel/Dto/DtoObjectExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapHive.Server.Core.DataModel
+{
+    public static class DtoObjectExtensions
+    {
+        /// <summary>
+        /// Copies public properties from a source object to a destination object
+        /// </summary>
+        /// <param name="dest"></param>
+        /// <param name="src"></param>
+        public static void CopyPublicPropertiesFrom(this object dest, object src)
+        {
+            var flags = BindingFlags.Instance | BindingFlags.Public;
+
+            var srcProps = src.GetType().GetProperties(flags);
+            var dstProps = dest.GetType().GetProperties(flags);
+
+            foreach (var srcFld in srcProps)
+            {
+                var dstFld = dstProps.FirstOrDefault(field => field.Name == srcFld.Name);
+
+                if (dstFld != null && srcFld.PropertyType == dstFld.PropertyType)
+                {
+                    dstFld.SetValue(dest, srcFld.GetValue(src));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Copies public properties from a src object to a destination object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="src"></param>
+        /// <param name="dest"></param>
+        /// <returns></returns>
+        public static T CopyPublicPropertiesToNew<T>(this object src) where T : class
+        {
+            var dest = (T)Activator.CreateInstance(typeof(T));
+            dest.CopyPublicPropertiesFrom(src);
+            return dest;
+        }
+    }
+}
