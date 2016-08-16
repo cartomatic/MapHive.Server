037c558f63d0c82eaa5f0d43035ef908f48d1d3a
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-08-16 18:39:31 +0200

    Password reset operational

---
+++ Core/Auth/ChangePasswordFromResetKey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrockAllen.MembershipReboot;
+using BrockAllen.MembershipReboot.Relational;
+
+namespace MapHive.Server.Core
+{
+    public partial class Auth
+    {
+        public static async Task<bool> ChangePasswordFromResetKey<TAccount>(
+            UserAccountService<TAccount> userAccountService, string newPass, string verificationKey)
+            where TAccount : RelationalUserAccount
+        {
+            return userAccountService.ChangePasswordFromResetKey(verificationKey, newPass);
+        }
+    }
+}
