82a7fb93c497896741deba0b64e25cac697b5097
Dominik Mikiewicz <dominik@cartomatic.pl>
2017-03-13 06:56:01 +0100

    org apps apis

---
+++ API/OrgControllers/ApplicationsOrgController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MapHive.Server.Core.API;
+using MapHive.Server.Core.API.Filters;
+using MapHive.Server.Core.DataModel;
+using MapHive.Server.Core.DAL.DbContext;
+
+namespace MapHive.Server.API.OrgControllers
+{
+    [RoutePrefix("organisations/{" + OrganisationContextAttribute.OrgIdPropertyName + "}/applications")]
+    public class ApplicationsOrgController : BaseApiOrganisatinCrudController<Application, MapHiveDbContext>
+    {
+        //this customises the connection string the db context gets instantiated with
+        public ApplicationsOrgController()
+            : base("MapHiveMeta")
+        {
+        }
+
+        /// <summary>
+        /// Reads applications accessible for an organisation
+        /// </summary>
+        /// <param name="organisationId"></param>
+        /// <param name="sort"></param>
+        /// <param name="filter"></param>
+        /// <param name="start"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("linkable")]
+        [ResponseType(typeof(IEnumerable<Team>))]
+        public async Task<IHttpActionResult> Get(Guid organisationId, string sort = null, string filter = null,
+            int start = 0,
+            int limit = 25)
+        {
+            try
+            {
+                var apps = await OrganisationContext.GetOrganisationLinkableApps(_dbCtx, sort, filter, start, limit);
+
+                if (apps != null)
+                {
+                    AppendTotalHeader(apps.Item2);
+                    return Ok(apps.Item1);
+                }
+
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return this.HandleException(ex);
+            }
+        }
+
+        
+    }
+}
--- Core/DataModel/Organisation/Applications.cs
+++ Core/DataModel/Organisation/Applications.cs
@@ -44,5 +44,81 @@ namespace MapHive.Server.Core.DataModel
         {
             return app.IsCommon || await this.HasChildLinkAsync(dbCtx, app);
         }
+
+
+        /// <summary>
+        /// Reads applications that are visible to an organisation and can be linked to it - returns apps that require auth and apps that are non-public (specific to that very org)
+        /// </summary>
+        /// <param name="dbCtx"></param>
+        /// <param name="sort"></param>
+        /// <param name="filter"></param>
+        /// <param name="start"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public async Task<Tuple<IEnumerable<Application>, int>> GetOrganisationLinkableApps(DbContext dbCtx, string sort = null, string filter = null,
+            int start = 0,
+            int limit = 25)
+        {
+            //first need to get objects for an organisation, and then add an extra filter with object guids
+            var orgObjIds = await GetOrganisationObjectIdsAsync<Application>(dbCtx);
+
+            //do make sure there is something to read!
+            var filters = filter.ExtJsJsonFiltersToReadFilters();
+
+
+            //return all the apps 
+            var preFilter = new ReadFilter
+            {
+                Property = nameof(Application.RequiresAuth),
+                Value = true,
+                Operator = "==",
+                ExactMatch = true, //make the whole filter exact
+                AndJoin = true,
+                NestedFilters = new List<ReadFilter>
+                {
+                    //need common apps
+                    new ReadFilter
+                    {
+                        Property = nameof(Application.IsCommon),
+                        Value = true
+                    },
+                    //but not the dashboard. users always can access the dashboard!
+                    new ReadFilter
+                    {
+                        Property = nameof(Application.IsDefault),
+                        Value = false
+                    }
+                }
+            };
+
+            //if there are org specific apps always return them in addition to the ones filtered by default
+            if (orgObjIds.Any())
+            {
+                preFilter = new ReadFilter
+                {
+                    Property = "Uuid",
+                    Value = orgObjIds.AsReadFilterList(),
+                    Operator = "in",
+                    ExactMatch = true,
+                    AndJoin = false,
+                    NestedFilters = new List<ReadFilter>
+                    {
+                        preFilter
+                    }
+                };
+            }
+
+            filters.Add(preFilter);
+
+
+            var app = new Application();
+            var apps = await app.ReadAsync(dbCtx, sort.ExtJsJsonSortersToReadSorters(), filters, start, limit);
+
+            if (!apps.Any()) return null;
+
+            var count = await apps.First().ReadCountAsync(dbCtx, filters);
+
+            return new Tuple<IEnumerable<Application>, int>(apps, count);
+        }
     }
 }
